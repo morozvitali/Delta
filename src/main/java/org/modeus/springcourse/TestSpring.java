f5b768a1992bcdbaa18496818f2b7f21e80273e3
morozvitali <morozvitali99@gmail.com>
2024-11-15 16:27:19 +0200

    Task 007 - scope testing

--- src/main/java/org/modeus/springcourse/TestSpring.java
+++ src/main/java/org/modeus/springcourse/TestSpring.java
@@ -8,14 +8,25 @@ public class TestSpring {
                 "applicationContext.xml"
         );
 
-        MusicPlayer musicPlayer = context.getBean("musicPlayer", MusicPlayer.class);
+        MusicPlayer firstMusicPlayer = context.getBean("musicPlayer", MusicPlayer.class);
+
+        MusicPlayer secondMusicPlayer = context.getBean("musicPlayer", MusicPlayer.class);
+
+        System.out.println(firstMusicPlayer==secondMusicPlayer);
+        System.out.println(firstMusicPlayer.toString());
+        System.out.println(secondMusicPlayer.toString());
+
+        firstMusicPlayer.setVolume(22);
+        secondMusicPlayer.setVolume(23);
+        System.out.println(secondMusicPlayer.getVolume());
+        System.out.println(firstMusicPlayer.getVolume());
 
         // Вивести інформацію про програвач
         //System.out.println("Player Name: " + musicPlayer.getName());
         //System.out.println("Volume: " + musicPlayer.getVolume());
 
         // Програти всі треки
-        musicPlayer.playAllMusic();
+       //musicPlayer.playAllMusic();
 
         context.close();
     }
